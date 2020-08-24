0fa2de847fd942a25561e8e34fb0b27af954a688
cameron coxen <coxecame@isu.edu>
2020-08-24 08:21:54 -0600

    Add project files.

---
+++ ConsoleFun.vb
@@ -0,0 +1,16 @@
+﻿Module ConsoleFun
+
+    Sub Main()
+        Dim userInput As String
+        Console.WriteLine("Please typem some text and press enter...")
+        userInput = Console.ReadLine()
+        Console.WriteLine("Thanks! you entered: " & userInput)
+        Console.WriteLine("Nice on!")
+        Console.ReadLine()
+
+
+
+
+    End Sub
+
+End Module
